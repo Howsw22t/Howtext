fc49099ab0f3ceecd9dfb9d3443b970d9cc2a0a4
howsw22t <rickrollsbest@gmail.com>
2026-05-02 20:22:37 +0900

    Save changes before rebase

--- Howtext/__init__.py
+++
@@ -1,4 +0,0 @@
-from .core import HowText
-
-__version__ = "0.1.0"
-__author__ = "Howsweet_0803"
--- howtext/__init__.py
+++ howtext/__init__.py
@@ -1,4 +1,4 @@
 from .core import HowText
 
-__version__ = "0.1.0"
+__version__ = "0.1.5"
 __author__ = "Howsweet_0803"
